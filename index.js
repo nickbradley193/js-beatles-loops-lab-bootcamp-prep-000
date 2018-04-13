5959bf800cd3494ee76014a424de7213ca938fab
Nick Bradley <nick.bradley.193@pm.me>
2018-04-13 01:08:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var resultArray;
   for (let n = 0; n < musicians.length; n++) {
-    resultArray[n] = `${musicians[n]} plays ${instruments[n]}`;
+    resultArray[n] = musicians[n] + ' plays ' + instruments[n];
   }
   return resultArray;
 }
